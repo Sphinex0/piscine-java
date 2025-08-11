fec02529b789913bcf9362024f13f50dfcf21c41
Sphinex0 <adnane.elmir1@gmail.com>
2025-08-11 13:53:39 +0100

    Palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,5 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        return s.equals(new StringBuilder(s).reverse().toString());
+        return s.equalsIgnoreCase(new StringBuilder(s).reverse().toString());
     }
 }
