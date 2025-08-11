9d9c93874c1d5e3ecb98c6dc09ca5fc65fa5a419
Sphinex0 <adnane.elmir1@gmail.com>
2025-08-11 14:06:58 +0100

    computer array

--- ComputeArray/ComputeArray.java
+++ ComputeArray/ComputeArray.java
@@ -3,14 +3,14 @@ import java.util.Arrays;
 public class ComputeArray {
     public static int[] computeArray(int[] array) {
         return Arrays.stream(array)
-        .map(e->{
-            if (e % 3==0){
-                return e*5;
-            }else if ((e-1)%3==0){
-                return e+7;
-            }else{
-                return e;
-            }
-        }).toArray();
+                .map(e -> {
+                    if (e % 3 == 0) {
+                        return e * 5;
+                    } else if ((e - 1) % 3 == 0) {
+                        return e + 7;
+                    } else {
+                        return e;
+                    }
+                }).toArray();
     }
 }
