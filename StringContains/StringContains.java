926b8fdfd369afc9433f7796111bdbe301ac4d7f
Sphinex0 <adnane.elmir1@gmail.com>
2025-08-11 12:39:40 +0100

    String contains

--- StringContains/StringContains.java
+++ StringContains/StringContains.java
@@ -1,5 +1,5 @@
 public class StringContains {
     public static boolean isStringContainedIn(String subString, String s) {
-        return s == null ? false : s.contains(subString == null ? "" : subString);
+        return s == null || subString == null ? false : s.contains(subString);
     }
 }
