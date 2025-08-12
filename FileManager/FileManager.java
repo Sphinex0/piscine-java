515d2ec67105e14bc5ecf96b0c9767a98e71b831
aelmir <adnane.elmir1@gmail.com>
2025-08-12 12:23:32 +0100

    capitalize

---
+++ FileManager/FileManager.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FileManager {
+    public static void createFile(String fileName, String content) throws IOException {
+        Files.writeString(Files.createFile(Paths.get(fileName)), content);
+    }
+
+    public static String getContentFile(String fileName) throws IOException {
+        Files.readString(Paths.get(fileName));
+    }
+
+    public static void deleteFile(String fileName) {
+        Files.deleteIfExists(Paths.get(fileName));
+    }
+}
