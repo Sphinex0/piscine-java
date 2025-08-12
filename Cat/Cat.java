4edda213344fdf789299735cec64f27dbd71c380
aelmir <adnane.elmir1@gmail.com>
2025-08-12 11:13:57 +0100

    cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -5,6 +5,7 @@ import java.nio.file.Paths;
 
 public class Cat {
     public static void cat(String[] args) throws IOException {
-        System.out.print(args.length == 0 ? "" :Files.readString(Paths.get(args[0])));
+        System.out.print(args.length == 0 ? "" : new String(Files.readAllBytes(Paths.get(args[0])))); 
+        // System.out.print(args.length == 0 ? "" :Files.readAllBytes(Paths.get(args[0])));
     }
 }
