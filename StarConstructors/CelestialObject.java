b456bb7877e5195e876502ddabc0e3f68f3060a7
aelmir <adnane.elmir1@gmail.com>
2025-08-13 10:07:26 +0100

    star constructors

---
+++ StarConstructors/CelestialObject.java
@@ -0,0 +1,19 @@
+
+public class CelestialObject {
+    public double x;
+    public double y;
+    public double z;
+    public String name;
+
+    public CelestialObject() {
+        name = "Soleil";
+    }
+
+    public CelestialObject(String name1, double x1, double y1, double z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+        name = name1;
+    }
+
+}
