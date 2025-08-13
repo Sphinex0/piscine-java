dd573ae5b60230d336bdd6785feb55dab84a4d9d
aelmir <adnane.elmir1@gmail.com>
2025-08-13 13:18:34 +0100

    star galaxy

--- StarGalaxy/Galaxy.java
+++ StarGalaxy/Galaxy.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Galaxy {
     private List<CelestialObject> celestialObjects;
 
     public Galaxy(){
-        celestialObjects = List.of();
+        celestialObjects = new ArrayList<CelestialObject>();
     }
 
     public List<CelestialObject> getCelestialObjects() {
