eb118578774acfca851301c173f8ea3daa5bad0d
aelmir <adnane.elmir1@gmail.com>
2025-08-18 11:28:53 +0100

    adventure charachtere

---
+++ AdventureCharacter/Character.java
@@ -0,0 +1,46 @@
+
+public class Character {
+    final int maxHealth;
+    int currentHealth;
+    final String name;
+
+    // constructor
+    public Character(String name, int maxHealth) {
+        this.maxHealth = maxHealth;
+        this.name = name;
+        this.currentHealth = maxHealth;
+    }
+
+    // getters
+    public int getCurrentHealth() {
+        return currentHealth;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return currentHealth == 0 ? String.format("%s : KO", this.name)
+                : String.format("%s : %d/%d", this.name, this.currentHealth, this.maxHealth);
+    }
+
+    // METHODS
+    public void takeDamage(int dmg) {
+        if (this.currentHealth - dmg < 0) {
+            this.currentHealth = 0;
+        } else {
+            this.currentHealth -= dmg;
+        }
+    }
+
+    public void attack(Character ch) {
+        ch.takeDamage(9);
+    }
+}
