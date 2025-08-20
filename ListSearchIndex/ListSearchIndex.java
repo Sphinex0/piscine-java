39b384a838ca6083eb2c4aeec033e16a42b85c05
aelmir <adnane.elmir1@gmail.com>
2025-08-20 10:47:52 +0100

    list search index

--- ListSearchIndex/ListSearchIndex.java
+++ ListSearchIndex/ListSearchIndex.java
@@ -5,14 +5,14 @@ public class ListSearchIndex {
     public static Integer findLastIndex(List<Integer> list, Integer value) {
         if (list == null)
             return null;
-        int index = list.indexOf(value);
+        int index = list.lastIndexOf(value);
         return index == -1 ? null : index;
     }
 
     public static Integer findFirstIndex(List<Integer> list, Integer value) {
         if (list == null)
             return null;
-        int index = list.lastIndexOf(value);
+        int index = list.indexOf(value);
         return index == -1 ? null : index;
     }
 
