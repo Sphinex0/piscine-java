cccb28cb3e5e3bed833abf66d2ac2186d5dce8dc
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-08-20 10:25:48 +0100

    list equals

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -2,6 +2,10 @@ import java.util.List;
 
 public class ListEquals {
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
+        if (list1 == null ^ list2 == null)
+            return false;
+        if (list1 == null && list2 == null)
+            return true;
         return list1.equals(list2);
     }
 }
