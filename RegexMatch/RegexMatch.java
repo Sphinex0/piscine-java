af69bd8951a3630475177ed5f8109cd86550943c
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-08-25 12:11:21 +0100

    regex match

---
+++ RegexMatch/RegexMatch.java
@@ -0,0 +1,16 @@
+import java.util.regex.Pattern;
+
+public class RegexMatch {
+    public static boolean containsOnlyAlpha(String s) {
+        return Pattern.matches("\\w+", s);
+    }
+    
+    public static boolean startWithLetterAndEndWithNumber(String s) {
+        // your code here
+        return Pattern.matches("\\w.*\\d", s);
+    }
+    
+    public static boolean containsAtLeast3SuccessiveA(String s) {
+        return Pattern.matches(".*AAA.*", s);
+    }
+}
