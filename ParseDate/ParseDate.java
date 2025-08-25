781563c184d77d25629d713a1a077748f57f538c
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-08-25 11:27:07 +0100

    parse date

--- ParseDate/ParseDate.java
+++ ParseDate/ParseDate.java
@@ -7,25 +7,24 @@ import java.util.Locale;
 public class ParseDate {
 
     public static LocalDateTime parseIsoFormat(String stringDate) {
+        if (stringDate == null)return null;
         return LocalDateTime.parse(stringDate);
     }
 
     public static LocalDate parseFullTextFormat(String stringDate) {
-        // lundi 25 avril 2022
+        if (stringDate == null)return null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.FRENCH);
         return LocalDate.parse(stringDate, formatter);
     }
 
     public static LocalTime parseTimeFormat(String stringDate) {
-    
-        // "09 heures du soir, 07 minutes et 23 secondes"
+        if (stringDate == null)return null;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("H 'heures du soir,' mm 'minutes et' ss 'secondes'",
                 Locale.FRENCH);
-        if (stringDate != null && stringDate.contains("heures du soir")) {
+        if (stringDate.contains("heures du soir")) {
             return LocalTime.parse(stringDate, formatter).plusHours(12);
 
         }
-
         return LocalTime.parse(stringDate, formatter);
     }
 
