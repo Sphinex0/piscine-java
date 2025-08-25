262259b5da521fb82a067be8e22939c1deec4e03
aelmir || Sphinex0 <adnane.elmir1@gmail.com>
2025-08-25 11:43:51 +0100

    parse date

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -8,6 +8,9 @@ public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
         // Le 22 août de l'an 2021 à 13h25m et 46s
+        if (dateTime == null)
+            return null;
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
                 "'Le' dd MMMM 'de l''an' yyyy 'à' HH'h'mm'm et' ss's'",
                 Locale.FRENCH);
@@ -15,11 +18,17 @@ public class FormatDate {
     }
 
     public static String formatSimple(LocalDate date) {
+        if (date == null)
+            return null;
+
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd YY", Locale.ITALIAN);
         return formatter.format(date);
     }
 
     public static String formatIso(LocalTime time) {
+        if (time == null)
+            return null;
+
         return DateTimeFormatter.ISO_TIME.format(time);
     }
 
